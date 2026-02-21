b303d8a66044b7c6ec57e3053215bc471e7aa8ab
alishihovyakub-gif <alishihovyakub@gmail.com>
2026-02-21 18:58:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -106,6 +106,5 @@ function processTrades(trades) {
   return result;
 }
 
-app.listen(3000, () => {
-  console.log('✅ Сервер запущен на порту 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Сервер запущен на порту ${PORT}`));
